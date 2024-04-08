37d79ae6adc9d429a12d71a1f2708fd95893942f
quxiaowei <quxiaowei.work@gmail.com>
2024-04-08 23:35:45 +0800

    bugfix: two many virtual canvases bug

--- src/main.js
+++ src/main.js
@@ -10,36 +10,42 @@ let color_back = dark_mode ? "rgb(50,50,50)" : "rgb(245,245,245)";
 let color_back2 = dark_mode ? "rgba(50,50,50,0.02)" : "rgba(245,245,245,0.02)";
 let canvas_size = []
 let bg;
-let legend_opened = false;
+let lo;
 
 function setup() {
     let width = sim.config.frameSize.width ? sim.config.frameSize.width : 600;
     let height = sim.config.frameSize.height ? sim.config.frameSize.height : 400;
 
-    height += sim.config.frameSize.width < 600? 140 : 100;
+    // height += sim.config.frameSize.width < 600 ? 140 : 100;
 
     canvas_size = [width, height];
 
     createCanvas(width, height);
-    
+
+    bg && bg.remove();
     bg = createGraphics(width, height);
-    
     bg.background(color_back);
+
     frameRate(60);
     frames = 0;
-    legend_opened = false;
-
+    if (legend) {
+        legend.isOpen = false;
+    }
 }
 
 function draw() {
-    legend();
     if (sim.stop) {
+        if (!legend.isOpen) {
+            image(legend(), 0, canvas_size[1] - (canvas_size[0] < 600 ? 130 : 100));
+            legend.isOpen = true;
+        }
         return;
     }
 
-    legend_opened = false;
-    
+    legend.isOpen = false;
+
     frames += 1
+
     if (frames % 60 === 0) {
         bg.background(color_back2);
     }
@@ -77,42 +83,32 @@ function grid() {
     }
 }
 
-function legend() {
-    if(!sim.stop){
-        legend_opened = false;
-        return;
-    }
-
-    if(legend_opened){
-        return;
-    }
-
-    let lo = createGraphics(canvas_size[0], canvas_size[1]);
-
+let legend = function () {
+    lo && lo.remove && lo.remove();
+    lo = createGraphics(width, height);
     lo.strokeWeight(0);
-    // lo.fill(color_back)
-    // lo.rect(0, 0, canvas_size[0], canvas_size[1]);
+    lo.textStyle(BOLDITALIC);
 
-    let l_color_back = dark_mode ? "rgba(255,255,255,0.05)" : "rgba(0,0,0,0.05)";
+    let l_color_back = dark_mode ? "rgba(255,255,255,0.08)" : "rgba(0,0,0,0.08)";
 
-    let xoffset = sim.config.frameSize.width < 600 ? 30 : 310; 
-    let yoffset = sim.config.frameSize.width < 600 ? 110 : 20; 
+    let xoffset = sim.config.frameSize.width < 600 ? 30 : 310;
+    let yoffset = sim.config.frameSize.width < 600 ? 110 : 20;
 
     lo.strokeWeight(0);
     lo.fill(l_color_back)
     lo.rect(0, 0, canvas_size[0], canvas_size[1]);
-    
+
     let y = 20;
     lo.strokeWeight(0);
-    lo.fill(color_fore2);
+    lo.fill("white");
 
     lo.text(`Mass`, 30, y);
     lo.text(`Velocity`, 80, y);
     lo.text(`Position`, 190, y);
-    
+
     lo.text(`G : ${sim.config.G}`, xoffset, yoffset);
-    lo.text(`Frame : ${frames}`, xoffset+50, yoffset);
-    lo.text(sim.ended ? `Ended` : `Paused`, xoffset+160, yoffset);
+    lo.text(`Frame : ${frames}`, xoffset + 50, yoffset);
+    lo.text(sim.ended ? `Ended` : `Paused`, xoffset + 160, yoffset);
 
     const round = function (num) {
         return Math.round(num * 100) / 100
@@ -125,19 +121,16 @@ function legend() {
         lo.point(15 + 5, y - 5);
 
         lo.strokeWeight(0);
-        lo.fill(color_fore2);
+        lo.fill("white");
         lo.text(e.mass, 30, y);
 
+        lo.fill("white");
         lo.text(`( ${round(e.velocityX)}, ${round(e.velocityY)} )`, 80, y);
         lo.text(`( ${round(e.positionX)}, ${round(e.positionY)} )`, 190, y);
     });
 
-    image(lo, 0, 400);
-
-    legend_opened = true;
+    return lo;
 }
-
-// legend = _.debounce(legend, 100, { 'maxWait': 100 });
 
 
 /////////////
